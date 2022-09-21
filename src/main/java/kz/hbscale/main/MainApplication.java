2a1349881d481df9b63e684ed58a957148b71472
Галихан Хамитов <galikhan.khamitov@zee.kz>
2022-09-21 13:08:28 +0600

    save pid in file

--- src/main/java/kz/hbscale/main/MainApplication.java
+++ src/main/java/kz/hbscale/main/MainApplication.java
@@ -5,7 +5,10 @@ import kz.hbscale.main.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -18,7 +21,9 @@ public class MainApplication {
 	private UserRepository userRepository;
 
 	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
+		SpringApplication app = new SpringApplication(MainApplication.class);
+		app.addListeners(new ApplicationPidFileWriter("./bin/shutdown.pid"));
+		app.run(args);
 	}
 
 
